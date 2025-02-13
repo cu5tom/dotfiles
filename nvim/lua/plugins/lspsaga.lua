b36a1a355e0c26c4c9335fd4741cb941bfb25828
enrico <enrico.krause@sandstein.de>
2025-02-13 09:02:32 +0100

    chore(nvim): add lspsaga config

---
+++ nvim/lua/plugins/lspsaga.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvimdev/lspsaga.nvim",
+    config = function ()
+      require("lspsaga").setup({
+        lightbulb = {
+          enable = false
+        }
+      })
+    end
+  }
+}
