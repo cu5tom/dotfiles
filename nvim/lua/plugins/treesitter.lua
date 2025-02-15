7c2b65b6b27befe9188506e32d69f0551b871cef
Enrico Krause <enrico.krause@gmail.com>
2025-02-15 09:42:04 +0100

    chore(nvim): update treesitter.lua

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -9,11 +9,13 @@ return {
     ensure_installed = {
       "bash",
       "go",
+      "html",
       "lua",
       "markdown",
       "markdown_inline",
       "regex",
       "sql",
+      "toml",
       "typescript",
       "vim",
       -- add more arguments for adding more treesitter parsers
