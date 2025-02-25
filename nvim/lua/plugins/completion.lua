2669f9ca30ba14e108a43848b6a9ce8a1d949729
Enrico Krause <enrico.krause@gmail.com>
2025-02-25 21:11:36 +0100

    wip(nvim): update completion.lua

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -12,13 +12,14 @@ return {
       { "onsails/lspkind.nvim" },
       { "saadparwaiz1/cmp_luasnip" }
     },
-    opts = function(_, opts)
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, {
-        name = "lazydev",
-        group_index = 0
-      })
-    end,
+    opts = {},
+    -- opts = function(_, opts)
+    --   opts.sources = opts.sources or {}
+    --   table.insert(opts.sources, {
+    --     name = "lazydev",
+    --     group_index = 0
+    --   })
+    -- end,
     config = function()
       local lspkind = require("lspkind")
       lspkind.init({})
@@ -66,7 +67,6 @@ return {
           })
         },
         sources = {
-          -- { name = "buffer" },
           { name = "buffer", keyword_length = 5, max_item_count = 5 },
           { name = "luasnip" },
           { name = "nvim_lsp" },
