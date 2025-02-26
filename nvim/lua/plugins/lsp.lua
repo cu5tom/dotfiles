0026ecd10b76d14a5d0bb46ad84c1149afb2e478
Enrico Krause <enrico.krause@gmail.com>
2025-02-26 14:13:23 +0100

    chore(nvim): update lsp configs

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -55,7 +55,7 @@ return {
             end
           end
 
-          -- local client = vim.lsp.get_client_by_id(event.data.client_id)
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
           -- --if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
           -- if client and client.server_capabilities.documentHighlightProvider then
           --   local highlight_augroup = vim.api.nvim_create_augroup("nvim-lsp-highlight", { clear = false })
@@ -260,4 +260,17 @@ return {
       })
     end
   },
+  {
+    "folke/trouble.nvim",
+    opts = {},
+    cmd = "Trouble",
+    keys = {
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Trouble: Diagnostics" },
+      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Trouble: Buffer Diagnostics" },
+      { "<leader>xs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Trouble: Symbols" },
+      { "<leader>xl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "Trouble: LSP Definitions / references / ..." },
+      { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Trouble: Location List" },
+      { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Trouble: Quickfix List" },
+    }
+  }
 }
