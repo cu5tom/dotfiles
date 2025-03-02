a4feec35142bbb3c7d13b6bfed2c4c6fb52dfd7c
Enrico Krause <enrico.krause@gmail.com>
2025-03-02 09:37:54 +0100

    chore(nvim): update core keymappings

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+vim.keymap.set({ 'n', 'x' }, 's', '<Nop>')
 
 local opts = { noremap = true, silent = true }
 
