20154081acd2390528be06ce9b2078463af7e805
Enrico Krause <enrico.krause@gmail.com>
2025-03-07 18:30:42 +0100

    chore(nvim): formatting floaterminal.lua

--- nvim/plugin/floaterminal.lua
+++ nvim/plugin/floaterminal.lua
@@ -1,10 +1,10 @@
-vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>", { desc ="Double escape from terminal" })
+vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Double escape from terminal" })
 
 local state = {
   floating = {
     buf = -1,
-    win = -1
-  }
+    win = -1,
+  },
 }
 
 local function create_floating_window(opts)
@@ -14,7 +14,6 @@ local function create_floating_window(opts)
 
   local col = math.floor((vim.o.columns - width) / 2)
   local row = math.floor((vim.o.lines - height) / 2)
-
 
   local buf = nil
   if vim.api.nvim_buf_is_valid(opts.buf) then
@@ -38,12 +37,10 @@ local function create_floating_window(opts)
   return { buf = buf, win = win }
 end
 
-local toggle_terminal = function ()
+local toggle_terminal = function()
   if not vim.api.nvim_win_is_valid(state.floating.win) then
-    state.floating = create_floating_window({ buf = state.floating.buf })
-    if vim.bo[state.floating.buf].buftype ~= "terminal" then
-      vim.cmd.term()
-    end
+    state.floating = create_floating_window { buf = state.floating.buf }
+    if vim.bo[state.floating.buf].buftype ~= "terminal" then vim.cmd.term() end
   else
     vim.api.nvim_win_hide(state.floating.win)
   end
@@ -51,4 +48,4 @@ end
 
 vim.api.nvim_create_user_command("Floaterminal", toggle_terminal, {})
 
-vim.keymap.set({ "n", "t" }, "<c-t>", toggle_terminal, {desc = "Toggle floating terminal" })
+vim.keymap.set({ "n", "t" }, "<c-t>", toggle_terminal, { desc = "Toggle floating terminal" })
