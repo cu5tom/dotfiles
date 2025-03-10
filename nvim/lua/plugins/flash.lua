ad74abbc03d8c1e1ba1a0bbaf728813ea21cd5dc
Enrico Krause <enrico.krause@gmail.com>
2025-03-10 09:25:28 +0100

    feat(nviml): add flash.nvim to play around with

---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash Jump" },
+      {
+        "S",
+        mode = { "n", "x", "o" },
+        function() require("flash").treesitter() end,
+        desc = "Flash Treesitter",
+      },
+      { "r", mode = { "o" }, function() require("flash").remote() end, desc = "Flash Remote" },
+      { "R", mode = { "x", "o" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      {
+        "<c-f>",
+        mode = { "c" },
+        function() require("flash").toggle() end,
+        desc = "Flash Toggle",
+      },
+    },
+  },
+}
