2f106379f6d8789e95b3b513c9d42bffc6809735
Enrico Krause <enrico.krause@gmail.com>
2025-03-16 08:23:44 +0100

    fix(nvim): fix typo

--- nvim/lua/plugins/autocompletion.lua
+++ nvim/lua/plugins/autocompletion.lua
@@ -135,7 +135,7 @@ return {
       cmp.setup.cmdline(":", {
         mapping = cmp.mapping.preset.cmdline(),
         sources = cmp.config.sources({
-          { name = "path " },
+          { name = "path" },
         }, {
           {
             name = "cmdline",
