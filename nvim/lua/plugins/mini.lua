3986407a27dbce6f87d183751efd7da8127b75c3
Enrico Krause <enrico.krause@gmail.com>
2025-03-22 09:39:19 +0100

    feat(nvim): disable mini.ai plugin

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -3,7 +3,18 @@ return {
   {
     "echasnovski/mini.nvim",
     config = function()
-      require("mini.ai").setup { n_lines = 500 }
+      -- local spec_treesitter = require("mini.ai").gen_spec.treesitter
+      -- require("mini.ai").setup {
+      --   custom_textobjects = {
+      --     -- ['"'] = { '"().-()"' },
+      --     F = spec_treesitter { a = "@function.outer", i = "@function.inner" },
+      --     o = spec_treesitter {
+      --       a = { "@conditional.outer", "@loop.outer" },
+      --       i = { "@conditional.inner", "@loop.inner" },
+      --     },
+      --   },
+      --   n_lines = 500,
+      -- }
       -- require("mini.basics").setup()
 
       -- require("mini.diff").setup()
