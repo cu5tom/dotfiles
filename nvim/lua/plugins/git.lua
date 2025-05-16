deb2a4990f292e584721df4d081f7bd56f0c848f
enrico <enrico.krause@sandstein.de>
2025-05-16 07:45:47 +0200

    feat(nvim): add git related plugins

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -5,4 +5,18 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     opts = {},
   },
+  {
+    "f-person/git-blame.nvim",
+    event = "VeryLazy",
+    opts = {
+      enabled = false,
+    },
+  },
+  {
+    "FabijanZulj/blame.nvim",
+    lazy = false,
+    config = function()
+      require('blame').setup {}
+    end,
+  },
 }
