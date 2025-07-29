d6d920aa79f83235b67bbe51dd88cd08b4271e1f
enrico <enrico.krause@sandstein.de>
2025-07-29 12:04:11 +0200

    feat(nvim): update SnacksPickerGitStatusUntracked hl color

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -3,7 +3,10 @@ return {
     "folke/snacks.nvim",
     lazy = false,
     priority = 1000,
-    init = function() vim.g.snacks_animate = false end,
+    init = function()
+      vim.api.nvim_set_hl(0, "SnacksPickerGitStatusUntracked", { link = "@diff.plus" })
+      vim.g.snacks_animate = false
+    end,
     ---@type snacks.Config
     opts = {
       bigfile = {},
