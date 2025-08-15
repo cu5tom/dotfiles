9291c74af526d4495356a948b1ef9332434498e9
Enrico Krause <enrico.krause@gmail.com>
2025-08-15 08:28:33 +0200

    wip(nvim): testing blink keymaps

--- nvim/lua/plugins/autocompletion.lua
+++ nvim/lua/plugins/autocompletion.lua
@@ -37,9 +37,27 @@ return {
       },
     },
     version = "1.*",
+    config = function ()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
     ---@module "blink.cmp"
     ---@type blink.cmp.Config
     opts = {
+      keymap = {
+        preset = 'none',
+        ["<C-c>"] = { "show", "show_documentation", "hide_documentation", "fallback" },
+        ["<C-e>"] = { "hide" },
+        ["<C-y>"] = { "select_and_accept" },
+        ["<M-Up>"] = { "select_prev", "fallback" },
+        ["<M-Down>"] = { "select_next", "fallback" },
+        ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+        ["<C-n>"] = { "select_next" , "fallback_to_mappings" },
+        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+        ["<Tab>"] = { "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+        ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+      },
       sources = {
         default = {
           "lsp",
