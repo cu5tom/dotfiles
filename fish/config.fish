e244f50c98a55ff5fd038240d6cb36a78ad8ad1e
Enrico Krause <enrico.krause@gmail.com>
2025-08-16 13:28:40 +0200

    feat: add fish

---
+++ fish/config.fish
@@ -0,0 +1,26 @@
+# if status is-interactive
+#     # Commands to run in interactive sessions can go here
+# end
+
+# Tmux
+abbr t tmux
+abbr tc 'tmux attach'
+abbr ta 'tmux attach -t'
+abbr tad 'tmux attach -d -t'
+abbr ts 'tmux new -s'
+abbr tl 'tmux ls'
+abbr tk 'tmux kill-session -t'
+
+# Editor
+abbr vim nvim
+abbr vi nvim
+abbr v nvim
+
+# Dev
+abbr gg lazygit
+abbr gb 'git checkout -b'
+abbr gc 'git commit'
+abbr gp 'git pull'
+abbr gP 'git push'
+
+starship init fish | source
