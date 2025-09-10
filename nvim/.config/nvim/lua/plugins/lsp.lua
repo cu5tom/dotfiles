53ab992114688482a9c4dcfbd3e340aecf1cd9cc
Enrico Krause <enrico.krause@gmail.com>
2025-09-10 11:03:55 +0200

    feat(nvim): add keymap for showing signature help

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -54,6 +54,7 @@ return {
             vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
           end
 
+          map("S", vim.lsp.buf.signature_help, "Signature help")
           map("<leader>cs", vim.lsp.buf.document_symbol, "Symbols")
           map("<leader>cr", vim.lsp.buf.rename, "Rename")
           map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
