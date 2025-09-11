2fea74635136607a5ff4fc8c5af22171265197f0
Enrico Krause <enrico.krause@gmail.com>
2025-09-11 09:30:09 +0200

    wip(nvim, flash): disable keymaps for now

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -11,13 +11,13 @@ return {
       }
     },
     keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash Jump" },
-      {
-        "S",
-        mode = { "n", "x", "o" },
-        function() require("flash").treesitter() end,
-        desc = "Flash Treesitter",
-      },
+      -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash Jump" },
+      -- {
+      --   "S",
+      --   mode = { "n", "x", "o" },
+      --   function() require("flash").treesitter() end,
+      --   desc = "Flash Treesitter",
+      -- },
       { "r", mode = { "o" }, function() require("flash").remote() end, desc = "Flash Remote" },
       { "R", mode = { "x", "o" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       {
