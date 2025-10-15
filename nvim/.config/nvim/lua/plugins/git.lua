e632c4460554f7e99e4812e53a80f318b310e22f
Enrico Krause <enrico.krause@gmail.com>
2025-10-15 08:39:24 +0200

    feat(nvim): trying neogit plugin

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,6 +1,24 @@
 ---@type LazySpec[]
 return {
   {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "folke/snacks.nvim"
+    },
+    config = function ()
+      local neogit = require("neogit")
+      neogit.setup({})
+
+      local opts = { silent = true, noremap = true }
+
+      vim.keymap.set("n", "<leader>gs", function()
+        neogit.open({ kind = "floating" })
+      end, opts)
+    end
+  },
+  {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
     opts = {},
