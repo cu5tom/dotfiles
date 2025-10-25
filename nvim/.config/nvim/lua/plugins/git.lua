912864c5e9b4939ad4e4c14434ff28e248b2b0f2
Enrico Krause <enrico.krause@gmail.com>
2025-10-25 16:41:32 +0200

    feat(nvim): add gitsigns keymappings

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -21,7 +21,50 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
-    opts = {},
+    opts = {
+      on_attach = function(bufnr)
+        local gitsigns = require "gitsigns"
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        map("n", "]c", function()
+          if vim.wo.diff then
+            vim.cmd.normal { "]c", bang = true }
+          else
+            gitsigns.nav_hunk "next"
+          end
+        end)
+
+        map("n", "[c", function()
+          if vim.wo.diff then
+            vim.cmd.normal { "[c", bang = true }
+          else
+            gitsigns.nav_hunk "prev"
+          end
+        end)
+
+        map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Stage Hunk" })
+        map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Reset Hunk" })
+        map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Preview Hunk" })
+        map("n", "<leader>hi", gitsigns.preview_hunk_inline, { desc = "Previe Hunk inline" })
+
+        map("n", "<leader>hb", function() gitsigns.blame_line { full = true } end, { desc = "Blame Line" })
+
+        map("n", "<leader>hd", gitsigns.diffthis, { desc = "Diff this" })
+        map("n", "<leader>hD", function() gitsigns.diffthis "~" end, { desc = "Diff this ~" })
+
+        map("n", "<leader>hQ", function() gitsigns.setqflist "all" end, { desc = "Set QFList all" })
+        map("n", "<leader>hq", gitsigns.setqflist, { desc = "Set QFList" })
+
+        map("n", "<leader>htb", gitsigns.toggle_current_line_blame, { desc = "Toggle Current Line Blame" })
+        map("n", "<leader>htw", gitsigns.toggle_word_diff, { desc = "Toggle Word Diff" })
+
+        map({ "o", "x" }, "ih", gitsigns.select_hunk, { desc = "Select Hunk" })
+      end,
+    },
   },
   {
     "f-person/git-blame.nvim",
