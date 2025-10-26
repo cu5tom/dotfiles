20925660c2b67d791d7b49eaca4fd450845ffa18
Enrico Krause <enrico.krause@gmail.com>
2025-10-26 10:15:36 +0100

    feat(nvim): update keymappings regarding git workflow

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -11,11 +11,9 @@ return {
       local neogit = require("neogit")
       neogit.setup({})
 
-      local opts = { silent = true, noremap = true }
-
-      vim.keymap.set("n", "<leader>gs", function()
+      vim.keymap.set("n", "<leader>gg", function()
         neogit.open({ kind = "floating" })
-      end, opts)
+      end, { desc = "Neogit", silent = true, noremap = true })
     end
   },
   {
@@ -46,23 +44,23 @@ return {
           end
         end)
 
-        map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Stage Hunk" })
-        map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Reset Hunk" })
-        map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Preview Hunk" })
-        map("n", "<leader>hi", gitsigns.preview_hunk_inline, { desc = "Previe Hunk inline" })
+        -- map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Stage Hunk" })
+        -- map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Reset Hunk" })
+        map("n", "<leader>gp", gitsigns.preview_hunk, { desc = "Preview Hunk" })
+        map("n", "<leader>gi", gitsigns.preview_hunk_inline, { desc = "Preview Hunk inline" })
 
-        map("n", "<leader>hb", function() gitsigns.blame_line { full = true } end, { desc = "Blame Line" })
+        map("n", "<leader>gB", function() gitsigns.blame_line { full = true } end, { desc = "Blame Line" })
 
-        map("n", "<leader>hd", gitsigns.diffthis, { desc = "Diff this" })
-        map("n", "<leader>hD", function() gitsigns.diffthis "~" end, { desc = "Diff this ~" })
+        -- map("n", "<leader>hd", gitsigns.diffthis, { desc = "Diff this" })
+        -- map("n", "<leader>hD", function() gitsigns.diffthis "~" end, { desc = "Diff this ~" })
 
         map("n", "<leader>hQ", function() gitsigns.setqflist "all" end, { desc = "Set QFList all" })
         map("n", "<leader>hq", gitsigns.setqflist, { desc = "Set QFList" })
 
-        map("n", "<leader>htb", gitsigns.toggle_current_line_blame, { desc = "Toggle Current Line Blame" })
-        map("n", "<leader>htw", gitsigns.toggle_word_diff, { desc = "Toggle Word Diff" })
+        map("n", "<leader>gb", gitsigns.toggle_current_line_blame, { desc = "Toggle Current Line Blame" })
+        -- map("n", "<leader>htw", gitsigns.toggle_word_diff, { desc = "Toggle Word Diff" })
 
-        map({ "o", "x" }, "ih", gitsigns.select_hunk, { desc = "Select Hunk" })
+        -- map({ "o", "x" }, "ih", gitsigns.select_hunk, { desc = "Select Hunk" })
       end,
     },
   },
