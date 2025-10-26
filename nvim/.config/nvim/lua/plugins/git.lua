365fbcd335378af7dc9f6e6e86979eb17f8564d8
Enrico Krause <enrico.krause@gmail.com>
2025-10-26 15:11:53 +0100

    feat(nvim): enable blame line as default

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -20,6 +20,7 @@ return {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
     opts = {
+      current_line_blame = true,
       on_attach = function(bufnr)
         local gitsigns = require "gitsigns"
         local function map(mode, l, r, opts)
