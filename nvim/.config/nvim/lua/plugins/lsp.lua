736ac1fb84d9576acb26cadeaeaf8b191585a33d
Enrico Krause <enrico.krause@gmail.com>
2025-10-27 09:01:15 +0100

    refactor(nvim, tiny-inline-diagnostic): move plugin config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -257,4 +257,22 @@ return {
     "antosha417/nvim-lsp-file-operations",
     config = {},
   },
+  ---@module "tiny-inline-diagnostic"
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    config = function()
+      require("tiny-inline-diagnostic").setup {
+        preset = "simple",
+        options = {
+          show_source = {
+            enabled = true,
+            if_many = true,
+          },
+        },
+      }
+      vim.diagnostic.config { virtual_text = false }
+    end,
+  },
 }
