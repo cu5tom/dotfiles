40950fb5ae007209dcddc5ec1c43d6ddf65247c9
Enrico Krause <enrico.krause@gmail.com>
2025-10-27 09:02:41 +0100

    feat(nvim): disable plugins

--- nvim/.config/nvim/lua/plugins/monorepo.lua
+++ nvim/.config/nvim/lua/plugins/monorepo.lua
@@ -1,12 +1,12 @@
 return {
-  {
-    "imNel/monorepo.nvim",
-    config = function()
-      require("monorepo").setup {}
-    end,
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "nvim-lua/plenary.nvim",
-    },
-  },
+  -- {
+  --   "imNel/monorepo.nvim",
+  --   config = function()
+  --     require("monorepo").setup {}
+  --   end,
+  --   dependencies = {
+  --     "nvim-telescope/telescope.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --   },
+  -- },
 }
