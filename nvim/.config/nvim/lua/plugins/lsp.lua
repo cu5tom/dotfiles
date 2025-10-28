6f8f958316ce24236b526be17b2201900f860203
Enrico Krause <enrico.krause@gmail.com>
2025-10-28 07:36:09 +0100

    wip(nvim): disable tiny-inline-diagnostic plugin

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -258,21 +258,21 @@ return {
     config = {},
   },
   ---@module "tiny-inline-diagnostic"
-  {
-    "rachartier/tiny-inline-diagnostic.nvim",
-    event = "VeryLazy",
-    priority = 1000,
-    config = function()
-      require("tiny-inline-diagnostic").setup {
-        preset = "simple",
-        options = {
-          show_source = {
-            enabled = true,
-            if_many = true,
-          },
-        },
-      }
-      vim.diagnostic.config { virtual_text = false }
-    end,
-  },
+  -- {
+  --   "rachartier/tiny-inline-diagnostic.nvim",
+  --   event = "VeryLazy",
+  --   priority = 1000,
+  --   config = function()
+  --     require("tiny-inline-diagnostic").setup {
+  --       preset = "simple",
+  --       options = {
+  --         show_source = {
+  --           enabled = true,
+  --           if_many = true,
+  --         },
+  --       },
+  --     }
+  --     vim.diagnostic.config { virtual_text = false }
+  --   end,
+  -- },
 }
