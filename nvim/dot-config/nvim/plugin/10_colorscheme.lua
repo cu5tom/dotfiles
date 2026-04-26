2fc1822d800eca6d697e90482129a886633f9fba
Enrico Krause <enrico.krause@gmail.com>
2026-04-26 10:51:00 +0200

    feat(nvim): tweak colorscheme

--- nvim/dot-config/nvim/plugin/10_colorscheme.lua
+++ nvim/dot-config/nvim/plugin/10_colorscheme.lua
@@ -1,6 +1,32 @@
 -- Colorscheme
 vim.pack.add({ "https://github.com/EdenEast/nightfox.nvim" })
 
-require("nightfox").setup()
+require("nightfox").setup({
+	options = {
+	  dim_inactive = true,
+		styles = {
+		  constants = "bold",
+			types = "italic,bold",
+		},
+	},
+	specs = {
+		nordfox = {
+			syntax = {
+			 --  builtin0 = "yellow",
+			 --  builtin1 = "black",
+			  builtin2 = "orange.bright",
+			  conditional = "magenta.bright",
+			  const = "orange.bright",
+			  ident = "yellow.dim",
+			 --  field = "blue",
+			  func = "blue.dim",
+			  keyword = "magenta.dim",
+			  statement = "black",
+				type = "yellow.bright",
+				-- variable = "white.bright"
+			},
+		},
+	},
+})
 
 vim.cmd.colorscheme("nordfox")
