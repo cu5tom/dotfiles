60abb9aa68e4113d1ca3c4d69febfa8cdd8b075f
Enrico Krause <enrico.krause@gmail.com>
2026-06-22 07:38:19 +0200

    feat(nvim): update keymappings

--- nvim/dot-config/nvim/lua/core/keymaps.lua
+++ nvim/dot-config/nvim/lua/core/keymaps.lua
@@ -12,7 +12,7 @@ vim.keymap.set("i", "jj", "<Esc>", opts)
 vim.keymap.set("i", "jk", "<Esc>", opts)
 
 -- Keep last yanked when pasting
-vim.keymap.set("v", "p", '"_dP')
+vim.keymap.set("x", "p", '"_dP')
 
 -- Clear highlights
 vim.keymap.set("n", "<Esc>", ":noh<CR>", opts)
@@ -22,14 +22,30 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
 vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
 
 -- Find and center
-vim.keymap.set("n", "n", "nzz", opts)
-vim.keymap.set("n", "N", "Nzz", opts)
+vim.keymap.set("n", "n", "nzzzv", opts)
+vim.keymap.set("n", "N", "Nzzzv", opts)
 
 -- Move window focus
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", opts)
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", opts)
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", opts)
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", opts)
+
+-- Indentation
+vim.keymap.set("v", "<", "<gv", opts);
+vim.keymap.set("v", ">", ">gv", opts);
+
+-- Join lines without moving cursor
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Replace word under cursor
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Native undotree
+vim.keymap.set("n", "<leader>u", function ()
+  vim.cmd.packadd("nvim.undotree")
+  require("undotree").open()
+end)
 
 -- Disable arrow keys
 local modes = { "n", "i", "o", "v", "t", "s", "x" }
