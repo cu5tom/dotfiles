1f1969dd65d484b42c7fcb800d2b0156aff140e0
Enrico Krause <enrico.krause@gmail.com>
2026-06-22 07:41:01 +0200

    feat(nvim): move require

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -1,3 +1,5 @@
+require("vim._core.ui2").enable({})
+
 require("core.keymaps")
 require("core.options")
 require("core.autocmd")
--- nvim/dot-config/nvim/lua/core/config.lua
+++ nvim/dot-config/nvim/lua/core/config.lua
@@ -27,5 +27,3 @@ vim.diagnostic.config({
 	end,
 })
 
--- test
-require("vim._core.ui2").enable({})
