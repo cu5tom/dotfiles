033ea6954104f00e64ac266e884261e64fdeb89e
Enrico Krause <enrico.krause@gmail.com>
2026-06-22 08:50:19 +0200

    chore(nvim): move vim.pack.add statement

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -5,5 +5,3 @@ require("core.keymaps")
 require("core.options")
 require("core.autocmd")
 require("core.config")
-
-vim.pack.add({ "https://github.com/nvim-mini/mini.misc" })
--- nvim/dot-config/nvim/lua/mini-loader.lua
+++ nvim/dot-config/nvim/lua/mini-loader.lua
@@ -1,3 +1,5 @@
+vim.pack.add({ "https://github.com/nvim-mini/mini.misc" })
+
 local misc = require("mini.misc")
 
 local M = {}
