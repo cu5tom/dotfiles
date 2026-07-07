85915b76da116c9b06b05d45d98cd38585b4166f
Enrico Krause <enrico.krause@gmail.com>
2026-07-07 08:22:25 +0200

    feat(nvim): update colorscheme

--- nvim/dot-config/nvim/plugin/10_colorscheme.lua
+++ nvim/dot-config/nvim/plugin/10_colorscheme.lua
@@ -1,24 +1,29 @@
 require("nightfox").setup({
 	options = {
-	  dim_inactive = true,
+		dim_inactive = true,
 		styles = {
-		  constants = "bold",
+			constants = "bold",
 			types = "italic,bold",
+		},
+	},
+	palettes = {
+		nordfox = {
+			comment = "#767677",
 		},
 	},
 	specs = {
 		nordfox = {
 			syntax = {
-			 --  builtin0 = "yellow",
-			 --  builtin1 = "black",
-			  builtin2 = "orange.bright",
-			  conditional = "magenta.bright",
-			  const = "orange.bright",
-			  ident = "yellow.dim",
-			 --  field = "blue",
-			  func = "blue.dim",
-			  keyword = "magenta.bright",
-			  statement = "black",
+				--  builtin0 = "yellow",
+				--  builtin1 = "black",
+				builtin2 = "orange.bright",
+				conditional = "magenta.bright",
+				const = "orange.bright",
+				ident = "yellow.dim",
+				--  field = "blue",
+				func = "blue.dim",
+				keyword = "magenta.bright",
+				statement = "black",
 				type = "yellow.bright",
 				-- variable = "white.bright"
 			},
