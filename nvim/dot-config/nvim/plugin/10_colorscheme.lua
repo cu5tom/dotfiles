7623748d0403d181d97233539df081e6db69d1a5
Enrico Krause <enrico.krause@gmail.com>
2026-07-17 09:44:08 +0200

    feat(nvim): reset colorscheme

--- nvim/dot-config/nvim/plugin/10_colorscheme.lua
+++ nvim/dot-config/nvim/plugin/10_colorscheme.lua
@@ -2,34 +2,39 @@ require("nightfox").setup({
 	options = {
 		dim_inactive = true,
 		styles = {
-		  comments = "italic",
-			constants = "bold",
-			types = "italic,bold",
+			-- comments = "italic",
+			-- constants = "bold",
+			-- types = "italic,bold",
 		},
 	},
 	palettes = {
 		nordfox = {
+		  -- bg1 = "#233440",
 			comment = "#878788",
 		},
 	},
 	specs = {
 		nordfox = {
 			syntax = {
-				--  builtin0 = "yellow",
-				--  builtin1 = "black",
-				builtin2 = "orange.bright",
-				conditional = "magenta.bright",
-				const = "orange.bright",
-				ident = "yellow.dim",
-				--  field = "blue",
-				func = "blue.dim",
-				keyword = "magenta.bright",
-				statement = "black",
-				type = "yellow.bright",
-				-- variable = "white.bright"
+				-- --  builtin0 = "yellow",
+				-- --  builtin1 = "black",
+				-- builtin2 = "orange.bright",
+				-- conditional = "magenta.bright",
+				-- const = "orange.bright",
+				-- ident = "yellow.dim",
+				-- --  field = "blue",
+				-- func = "blue.dim",
+				-- keyword = "magenta.bright",
+				-- statement = "black",
+				-- type = "yellow.bright",
+				-- -- variable = "white.bright"
 			},
 		},
 	},
 })
 
 vim.cmd.colorscheme("nordfox")
+
+-- vim.pack.add({ "https://github.com/vague-theme/vague.nvim" })
+-- require("vague").setup({ transparent = true })
+-- vim.cmd.colorscheme("vague")
