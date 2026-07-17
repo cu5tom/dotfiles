1a44aa55dbec2b1df078b56b2cf482a098200666
enrico <enrico.krause@sandstein.de>
2026-07-17 16:08:19 +0200

    feat(nvim): update formatting

--- nvim/dot-config/nvim/plugin/51_formatting.lua
+++ nvim/dot-config/nvim/plugin/51_formatting.lua
@@ -6,6 +6,8 @@ require("conform").setup({
 	formatters_by_ft = {
 		astro = { "oxfmt" },
 		css = { "oxfmt" },
+		html = { "oxfmt" },
+		htmlangular = { "oxfmt" },
 		htmldjango = { "djlint" },
 		javascript = { "oxfmt" },
 		javascriptreact = { "oxfmt" },
