c50cb219f411adc88bcc6fbdcc45ee66b81aac81
Enrico Krause <enrico.krause@gmail.com>
2026-08-06 11:18:17 +0200

    feat(neovim): update opencode config, add keymappings
    convenient shortcut for lua vim.pack.update() on the cmd line

--- nvim/dot-config/nvim/plugin/opencode.lua
+++ nvim/dot-config/nvim/plugin/opencode.lua
@@ -1,11 +1,40 @@
-local loader = require("mini-loader")
+vim.pack.add({
+	{ src = "https://github.com/nickvandyke/opencode.nvim", version = vim.version.range("*") },
+})
 
-loader.later(function()
-	vim.pack.add({ "https://github.com/NickvanDyke/opencode.nvim" })
+local opencode_cmd = "opencode --port"
 
-	-- require("opencode").setup()
+local snacks_terminal_opts = {
+  win = {
+    position = "right",
+    enter = false
+  }
+}
 
-	vim.keymap.set("n", "<leader>ot", function()
-		require("opencode").toggle()
-	end)
+vim.g.opencode_opts = {
+  server = {
+    start = function ()
+      require("snacks.terminal").open(opencode_cmd, snacks_terminal_opts)
+    end
+  }
+}
+
+vim.keymap.set({ "n", "t" }, "<M-t>", function ()
+  require("snacks.terminal").toggle(opencode_cmd, snacks_terminal_opts)
+end)
+
+vim.keymap.set({ "n", "x" }, "<M-a>", function ()
+  require("opencode").ask("@this: ")
+end)
+
+vim.keymap.set({ "n", "x" }, "<M-x>", function ()
+  require("opencode").select()
+end)
+
+vim.keymap.set({ "n", "x" }, "go", function ()
+  return require("opencode").operator("@this ")
+end)
+
+vim.keymap.set({ "n" }, "goo", function ()
+  return require("opencode").operator("@this ") .. "_"
 end)
